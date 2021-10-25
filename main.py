1ab5c0f82cd76d8b44c2dfeea5d873b7615427f7
noircodes <ilhamizdhihar2007@gmail.com>
2021-10-25 16:18:09 +0700

    Completely change the codes
    Make it simple!

--- main.py
+++ main.py
@@ -1,22 +1,18 @@
 import random
+import operator
 
 #define the variable
-n = random.sample(range(0,100), 5)
-
-
-
+n = random.sample(range(1,100), 2)
+ops = {">": operator.ge, "<": operator.le}
+iterator = iter(n)
 #Intro of the game
 print("Welcome to the game 'Higher or Lower' !")
-
+print("First number is " + str(n[0]) + ". Guess next number, Higher or Lower")
 #Game Start
-def begin():
-    print("First number is " + str(n[0]) + ". Guess next number, Higher or Lower")
-    hilo = input("Enter Higher or Lower (>/<): ")
-    y = n[+1]
-    result = str(y) + hilo + str(n)
-    
-begin()
-if  result == True:
-    begin()
+hilo = input("Enter Higher or Lower (>/<): ")
+print(n)
+result = ops[hilo](next(iterator),next(iterator))
+if result == True:
+    print("You Win!")
 else:
     print("You Lose!")
